33223c78b6aed6f004ff452f458393eb58681fc1
Vadim Firsov <4firsov@gmail.com>
2019-04-02 18:13:04 +0300

    Set up Main view controller table view

---
+++ SoundCloudApiIntegration/MainVC.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SoundCloudApiIntegration
+//
+//  Created by VADIM FIRSOV on 02/04/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MainVCCell.CELL_ID, for: indexPath) as? MainVCCell else { return UITableViewCell() }
+        cell.songNameLabel.text = "lol just for now thoooooooooooooo"
+        return cell
+    }
+
+
+}
+
---
+++ SoundCloudApiIntegration/MainVCCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainVCCell.swift
+//  SoundCloudApiIntegration
+//
+//  Created by VADIM FIRSOV on 02/04/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import UIKit
+
+class MainVCCell: UITableViewCell {
+    
+    @IBOutlet weak var songNameLabel: UILabel!
+    @IBOutlet weak var songImageView: UIImageView!
+    
+    static let CELL_ID = "MainVCCell"
+}
