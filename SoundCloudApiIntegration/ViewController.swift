95519389304042d972476c0d33851da5d159ef37
Vadim Firsov <4firsov@gmail.com>
2019-04-02 17:08:57 +0300

    Initial Commit

---
+++ SoundCloudApiIntegration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundCloudApiIntegration
+//
+//  Created by VADIM FIRSOV on 02/04/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
