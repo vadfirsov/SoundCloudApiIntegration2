fe1842a8bdc3c1c5a7dc5df6ccc8a1a6acf858c9
Vadim Firsov <4firsov@gmail.com>
2019-04-04 09:12:55 +0300

    Making a controller for paging programatically

---
+++ SoundCloudApiIntegration/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SoundCloudApiIntegration
+//
+//  Created by VADIM FIRSOV on 03/04/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let NO_IMG = "noImage"
+    static let GO_TO_PLAYERVC_ID = "goToPlayerVC"
+    
+}
